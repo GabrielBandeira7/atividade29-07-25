f39c5ef9f96507926156ac20f2539509e1599a65
GabrielBandeira7 <bandeiragabriel07@gmail.com>
2025-07-30 15:10:52 -0300

    Questão 2
    Questões JavaScript

---
+++ Atividade2A.js
@@ -0,0 +1,16 @@
+let nota1;
+let nota2;
+let nota3;
+let media;
+
+console.log("Insira a sua primeira nota: ", nota1);
+console.log("Insira a sua primeira nota: ", nota2);
+console.log("Insira a sua primeira nota: ", nota3);
+
+media = (nota1 + nota2 + nota3) / 3;
+
+if(media >= 7){
+    console.log("Aprovado!");
+}else{
+    console.log("Reprovado!");  
+}
