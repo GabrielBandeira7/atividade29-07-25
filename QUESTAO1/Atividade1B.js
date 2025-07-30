a5ced4230f40bb708b3715594c1e2fe2c238f143
GabrielBandeira7 <bandeiragabriel07@gmail.com>
2025-07-30 16:06:26 -0300

    Update Atividade1B.js

--- QUESTAO1/Atividade1B.js
+++ QUESTAO1/Atividade1B.js
@@ -5,12 +5,10 @@ let numeroB = Number(prompt("Digite o segundo número:"));
 
 console.log("Números digitados:", numeroA, "e", numeroB);
 
-// Operações matemáticas
 console.log("Soma:", numeroA + numeroB);
 console.log("Subtração:", numeroA - numeroB);
 console.log("Multiplicação:", numeroA * numeroB);
 
-// Verifica divisão por zero antes de calcular
 if (numeroB === 0) {
   console.log("Divisão: Impossível dividir por zero!");
   console.log("Resto da divisão: Impossível calcular!");
