48de39b424ebc49ac63bdae050c264af6074b9a5
GabrielBandeira7 <bandeiragabriel07@gmail.com>
2025-07-30 17:53:56 -0300

    Update Atividade2D.js

---
+++ Atividade2D.js
@@ -0,0 +1,14 @@
+const prompt = requise('prompt-sync')();
+
+let idade = 18;
+let sexopessoa;
+
+console.log("Digite sua idade: ", idade);
+console.log("Digite o seu sexo: ", sexopessoa);
+
+if(sexopessoa == "Masculino" && idade == 18){
+
+console.log("Precisa realizar o alistamento militar");
+}else{
+console.log("Não precisa");
+} 
