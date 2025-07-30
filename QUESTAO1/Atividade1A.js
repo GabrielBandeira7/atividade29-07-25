3942560a0af04ba971f794186838284f25f03025
GabrielBandeira7 <bandeiragabriel07@gmail.com>
2025-07-30 16:10:30 -0300

    Update Atividade1A.js

--- QUESTAO1/Atividade1A.js
+++ QUESTAO1/Atividade1A.js
@@ -7,3 +7,5 @@ let altura;
 nome = prompt("Digite seu nome: ");
 idade = prompt("Digite a sua idade: ");
 altura = prompt("Digite a sua altura: ");
+
+console.log("Meu nome é", nome, "tenho", idade, "e", altura, "metros de altura");
